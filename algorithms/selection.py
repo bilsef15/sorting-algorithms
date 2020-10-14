05bbe8ef2c991d0030398ff2a94f9dc2179049cb
bilsef15 <70355678+bilsef15@users.noreply.github.com>
2020-10-13 22:08:09 -0500

    Added comments

--- algorithms/selection.py
+++ algorithms/selection.py
@@ -1,9 +1,18 @@
+#implements selection sort
+
+#selection sort divides the list into a sorted and unsorted part
+#it finds the next lowest value and places it at the end of the sorted list
+
+#including the swap function
 import tools.swap as sw
 
 def selectionSort(data: list) -> list:
+    #loop from left to right
     for i in range(0, len(data)-1):
-        minIndex = i
+        minIndex = i #assume first index of unsorted array is minimum
+        #loop from left side of unsorted list to end of sorted list
         for j in range(i+1, len(data)-1):
+            #swap the lowest value in unsorted with the end of the sorted list
             if (data[j] < data[minIndex]):
                 minIndex = j
         sw.swap(data, minIndex, i)
--- tools/swap.py
+++ tools/swap.py
@@ -1,4 +1,6 @@
-#swap
+#it swaps two elements in a list and returns the list
+#This is trivial in python, but functions for this are often included in
+#more strongly typed languages
 
 def swap(data: list, index1: int, index2: int) -> list:
     data[index1], data[index2] = data[index2], data[index1]
