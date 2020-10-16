be709107d95d9b7734147c35eeb3f793f99cfd34
bilsef15 <70355678+bilsef15@users.noreply.github.com>
2020-10-15 22:38:46 -0500

    Added docstrings, removed debugging prints

--- algorithms/quick.py
+++ algorithms/quick.py
@@ -1,7 +1,29 @@
+'''Implementation of quick sort'''
 import tools.tools as tools
 
 
-def partition(data: list, low: int, high: int):
+def partition(data: list, low: int, high: int) -> int:
+    '''
+    The partition function of quick sort. It uses the 'high' element as
+    the pivot. It rearranges the array so that all values to the left (starting
+    from low) are less than the pivot and all values to the right are greater
+    (up to high.)
+    
+    Parameters
+    ----------
+    data : list
+        The list to be partitioned.
+    low : int
+        The lowest index to be affected.
+    high : int
+        The highest index to be affected.
+
+    Returns
+    -------
+    int
+        The index of the pivot value.
+
+    '''
     pivot = data[high]
     i = low - 1
     for j in range(low, high):
@@ -11,11 +33,27 @@ def partition(data: list, low: int, high: int):
     tools.swap(data, i+1, high)
     return i+1
     
-def quickSort(data: list, low: int, high: int):
+def quickSort(data: list, low: int, high: int) -> list:
+    '''
+    Sorts the list via quicksort.
+
+    Parameters
+    ----------
+    data : list
+        The list to be sorted.
+    low : int
+        Starting index.
+    high : int
+        Ending index.
+
+    Returns
+    -------
+    data : list
+        The sorted list from low to high.
+
+    '''
     if low < high:
-        pivot = partition(data, low, high)
-        print(f"Pivot: {pivot}   High: {high}   Low: {low}")
-        print(data)
-        quickSort(data, low, pivot-1)
-        quickSort(data, pivot+1, high)
+        part = partition(data, low, high)
+        quickSort(data, low, part-1)
+        quickSort(data, part+1, high)
     return data
