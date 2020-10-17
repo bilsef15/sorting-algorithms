701b6757ffae4ea20985f4bbeb740ea1327dbac4
bilsef15 <70355678+bilsef15@users.noreply.github.com>
2020-10-17 13:04:46 -0500

    Refactor and added Docstring  - swap -> tools

--- algorithms/insertion.py
+++ algorithms/insertion.py
@@ -1,11 +1,26 @@
-# insertion sorting algorithm
-import swap
+'''Implementation of insertion sort algorithm'''
+
+import tools.tools as tools
 
 def insertionSort(data: list) -> list:
+    '''
+    Sorts the list via insertion sort.
+
+    Parameters
+    ----------
+    data : list
+        The list to be sorted.
+
+    Returns
+    -------
+    list
+        The sorted list.
+
+    '''
     for i in range(1, len(data)):
         key = i
         while (key >= 1) and (data[key-1] > data[key]):
-            data = swap.swap(data, key, key-1)
+            data = tools.swap(data, key, key-1)
             key-=1
     return data
         
