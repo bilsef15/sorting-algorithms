505fce70b7128dfb9f028fad80d783a521b7b370
bilsef15 <70355678+bilsef15@users.noreply.github.com>
2020-10-17 13:00:54 -0500

    Refactored and added docstrings  - swap -> tools

--- algorithms/bubble.py
+++ algorithms/bubble.py
@@ -1,13 +1,28 @@
-import swap
+'''Implementation of bubble sort'''
 
+import tools.tools as tools
 
 def bubbleSort(data: list) -> list:
+    '''
+    Sorts the given list using bubble sort.
+
+    Parameters
+    ----------
+    data : list
+        The list to be sorted.
+
+    Returns
+    -------
+    list
+        The sorted list.
+
+    '''
     length = len(data)
     for i in range (0, length-1):
         currIndex = 0
         while (currIndex < length - i - 1):
             print(data)
             if (data[currIndex] > data[currIndex+1]):
-                data = swap.swap(data, currIndex, currIndex+1)
+                data = tools.swap(data, currIndex, currIndex+1)
             currIndex += 1
     return data
