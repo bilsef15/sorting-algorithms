5bfabec93d6613fe9e1299dc4125b930a28a7dd9
bilsef15 <70355678+bilsef15@users.noreply.github.com>
2020-11-01 22:29:51 -0600

    Added menuTest.py  - Created as a test for the robust, reusable menu module

---
+++ menuTest.py
@@ -0,0 +1,29 @@
+import pygame
+import tools.menu as menu
+
+
+def clicked():
+    
+    position = pygame.mouse.get_pos()
+    print(f'You clicked a button! x={position[0]}, y={position[1]}')
+
+pygame.init()
+screen = pygame.display.set_mode([800,800])
+
+mainMenu = menu.Menu(screen)
+
+mainMenu.addTitle("Test Title 1", 0.0, 0.0, 0.2, 0.1)
+mainMenu.addTitle("Test Title 2", 0.4, 0.0, 0.2, 0.1)
+mainMenu.addTitle("Test Title 3", 0.8, 0.0, 0.2, 0.1)
+
+for x in range(0, 10):
+    for y in range(1, 10):
+        mainMenu.addButton(f'Test {x + y*10}', x/10, y/10, 0.08, 0.08, clicked)
+        
+while True:
+    mainMenu.run()
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        
