7a189bbe505c36c32d76079c2977f41e3c9ba160
bilsef15 <70355678+bilsef15@users.noreply.github.com>
2020-11-20 20:04:22 -0600

    Add the "Sorting Algorithms" title

--- visualizer.py
+++ visualizer.py
@@ -15,6 +15,7 @@ screen = pygame.display.set_mode([800,800])
 sv = SV.sortVisualizer(screen, 60)
 
 mainMenu = menu.Menu(screen)
+mainMenu.addText("Sorting Algorithms", 0.344, 0.05, 0.313, 0.1)
 mainMenu.addButton("Bubble Sort", 0.02, 0.2, 0.313, 0.1, sv.bubbleSort)
 mainMenu.addButton("Insertion Sort", 0.344, 0.2, 0.313, 0.1, sv.insertionSort)
 mainMenu.addButton("Quick Sort", 0.667, 0.2, 0.313, 0.1, sv.quickSort)
